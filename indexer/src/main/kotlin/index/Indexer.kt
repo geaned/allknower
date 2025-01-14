8aa3cbfcb60ea40c2316d0ef90a5fea8b2dee247
Stanislav Mikhailov <stas_astr2002@mail.ru>
2025-01-14 22:03:51 +0400

    precommit

--- indexer/src/main/kotlin/index/Indexer.kt
+++ indexer/src/main/kotlin/index/Indexer.kt
@@ -25,14 +25,45 @@ class Indexer(
 
     fun updateIndex(documents: List<WikiDocument>) {
         for (document in documents) {
-            for (content in document.contents) {
+            // image
+            for (image in document.images) {
                 val doc = Document()
 
+                // image
+                try {
+                    doc.add(Field("doc_id", image.crc64, StringField.TYPE_STORED))
+                    doc.add(Field("page_url", document.pageUrl, StringField.TYPE_STORED))
+                    doc.add(Field("title", document.title, StringField.TYPE_STORED))
+                    doc.add(Field("metadata_title", image.metadata.title, StringField.TYPE_STORED))
+                    doc.add(Field("metadata_description", image.metadata.description, TextField.TYPE_STORED))
+                    doc.add(Field("embedding", image.embedding.joinToString(separator = " "), TextField.TYPE_STORED)) // TODO: check if exists and fix
+                } catch (e: Exception) {
+                    logger.error { "Failed while adding to index image, skip it. DocID: ${document.docId}. CRC64: ${image.crc64}. Error ${e.message}" }
+                    continue
+                }
+
+                writer.addDocument(doc)
+
+                logger.info { "Image added to index. DocID: ${document.docId}. CRC64: ${image.crc64}" }
+            }
+
+            // text
+            for (content in document.contents) {
+                val doc = Document()
+                // vector search
+                doc.add(Field("doc_id", content.contentId, StringField.TYPE_STORED))
+                doc.add(Field("title", document.title, StringField.TYPE_STORED))
+                doc.add(Field("page_url", document.pageUrl, StringField.TYPE_STORED))
+                doc.add(Field("content", content.content, StringField.TYPE_STORED))
+                doc.add(Field("embedding", content.embedding.joinToString(separator = " "), TextField.TYPE_STORED)) // TODO: check if exists and fix
+
+                // full-text search
                 try {
                     doc.add(Field("doc_id", content.contentId, StringField.TYPE_STORED))
-                    doc.add(Field("title", document.title, StringField.TYPE_NOT_STORED))
+                    doc.add(Field("title", document.title, StringField.TYPE_STORED))
                     doc.add(Field("page_url", document.pageUrl, StringField.TYPE_STORED))
                     doc.add(Field("content", content.content, TextField.TYPE_STORED))
+                    doc.add(Field("embedding", content.embedding.joinToString(separator = " "), TextField.TYPE_NOT_STORED)) // TODO: check if exists and fix
                     doc.add(
                         Field(
                             "keywords",
